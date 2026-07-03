a7b96e38fc44ab8bd76942a43940f7b07c989767
Abstract <21150449+abstractpotato@users.noreply.github.com>
2026-07-03 19:47:43 -0400

    Jul 03, 2026 - 07:47 PM

--- ledger/memory.go
+++ ledger/memory.go
@@ -1,7 +1,22 @@
 package ledger
 
-import "github.com/abstractpotato/potato-serialization-lib/psl"
+import (
+  "sync"
+  PSL "github.com/abstractpotato/potato-serialization-lib/psl"
+)
 
 type Memory struct {
-  
+  Lock         sync.Mutex
+  Self         string
+  Peers        []string
+  Context      PSL.Context
+  Blocks       map[uint]PSL.Block
+  Transactions map[string]PSL.Transaction
+}
+
+func NewMemory() Memory {
+  return Memory{
+    Blocks: make(map[uint]PSL.Block, 0),
+    Transactions: make(map[string]PSL.Transaction, 0),
+  }
 }
