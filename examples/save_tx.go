16aea0ad87421b20f7412499950e0d96f31925e3
Abstract <21150449+abstractpotato@users.noreply.github.com>
2026-07-29 14:34:02 -0400

    Jul 29, 2026 - 02:34 PM

--- examples/save_tx.go
+++ examples/save_tx.go
@@ -2,23 +2,22 @@ package main
 
 import (
   "fmt"
-  PSL "github.com/abstractpotato/potato-serialization-lib/psl"
-  Builders "github.com/abstractpotato/potato-serialization-lib/builders"
-  Ledger   "github.com/abstractpotato/starch-pay-ledger/ledger"
+  "github.com/abstractpotato/potato-serialization-lib"
+  "github.com/abstractpotato/starch-pay-ledger/ledger"
 )
 
 func main() {
   // sample param data
-  params := PSL.NewParams()
+  params := psl.NewParams()
   params.Network = 0
   params.MaxTxSize = 4000
   params.TxFeePerByte = 430
   params.MinTxFee = params.TxFeePerByte * 175 // signature size
 
-  txBuilder := Builders.NewTxBuilder()
+  txBuilder := psl.NewTxBuilder()
   txBuilder.Params = params
 
-  output := PSL.SimpleOutput{}
+  output := psl.SimpleOutput{}
   output.To = "target_cardano_addr"
   output.Asset = "policy_id+asset_name"
   output.Amount = 10000
@@ -39,7 +38,7 @@ func main() {
   txCBOR, err = disk.GetTxCBOR(txHash)
   if err != nil { panic(err) }
 
-  tx, err := PSL.TransactionFromCBOR(txCBOR)
+  tx, err := psl.TransactionFromCBOR(txCBOR)
   if err != nil { panic(err) }
 
   // passed if loads the same hash
